2e65d699d245b2f9e6c6e666bdefd438868a7fee
Emma Cadavra <emmacadavra@gmail.com>
2024-02-16 18:55:44 +0000

    Remove React logo etc from App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-import logo from './logo.svg';
 import { Button } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello, world!
-        </p>
-        <p>
-          <Button variant="warning">Warning</Button>
-        </p>
-      </header>
+      <p>
+        Hello, world!
+      </p>
+      <p>
+        <Button variant="warning">Warning</Button>
+      </p>
     </div>
   );
 }
