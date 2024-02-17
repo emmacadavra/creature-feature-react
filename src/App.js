75412fe27f4e9d734019285283c7f3b78b6944bd
Emma Cadavra <emmacadavra@gmail.com>
2024-02-17 14:00:20 +0000

    Amend favicon files, add App.module.css and NavBar.module.css to provide basic styling (including Google Fonts)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import { Button } from "react-bootstrap";
-import "./App.css";
+import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <NavBar />
-      <p>Hello, world!</p>
-      <p>
-        <Button variant="warning">Warning</Button>
-      </p>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,20 +2,34 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import styles from "../styles/NavBar.module.css";
 import cf_logo_small from "../assets/cf_logo_small.png";
+import home from "../assets/home.png";
+import login from "../assets/login.png";
+import register from "../assets/register.png";
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" fixed="top">
+    <Navbar expand="md" fixed="top" className={styles.NavBar}>
       <Container>
         <Navbar.Brand>
-          <img src={cf_logo_small} alt="Logo" height="50" />
+          <img src={cf_logo_small} alt="Logo" height="60" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+          <Nav className="ms-auto text-end">
+            <Nav.Link>
+              <img src={home} alt="Home" height="32" />
+              Home
+            </Nav.Link>
+            <Nav.Link>
+              <img src={login} alt="Sign In" height="32" />
+              Sign In
+            </Nav.Link>
+            <Nav.Link>
+              <img src={register} alt="Sign Up" height="32" />
+              Sign Up
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
