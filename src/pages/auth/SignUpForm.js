1b225ea8e30c72d3b7ffce13a84bd666d5f7646e
Emma Cadavra <emmacadavra@gmail.com>
2024-02-17 20:36:42 +0000

    Create handleChange and handleSubmit functions, populate form fields to interact with API and display alerts. Receiving 404 errors in console when trying to submit form.

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,12 +1,42 @@
-import React from "react";
-import { Form, Button, Col, Row, Container } from "react-bootstrap";
+import React, { useState } from "react";
+import { Form, Button, Col, Row, Container, Alert } from "react-bootstrap";
 import styles from "../../styles/SignInUpForm.module.css";
 import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import cf_text_logo from "../../assets/cf_text_logo.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const SignUpForm = () => {
+  const [signUpData, setSignUpData] = useState({
+    username: "",
+    password1: "",
+    password2: "",
+  });
+  const { username, password1, password2 } = signUpData;
+
+  const [errors, setErrors] = useState({});
+
+  const navigate = useNavigate();
+
+  const handleChange = (event) => {
+    setSignUpData({
+      ...signUpData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("/dj-rest-auth/registration/", signUpData);
+      navigate("/signin");
+    } catch (err) {
+      setErrors(err.response?.data);
+      console.log(err);
+    }
+  };
+
   return (
     <Row className={`justify-content-center ${styles.Row}`}>
       <Col xs={12} m={8} lg={6}>
@@ -16,40 +46,86 @@ const SignUpForm = () => {
             alt="Creature Feature"
             className={styles.SignInUpLogo}
           />
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group controlId="username">
               <Form.Label className="d-none">Enter a username</Form.Label>
               <Form.Control
                 type="text"
                 placeholder="Enter a username"
                 name="username"
+                value={username}
+                onChange={handleChange}
                 className={`mt-2 ${styles.FormInput}`}
               />
             </Form.Group>
+            {errors.username?.map((message, idx) => (
+              <Alert
+                variant="info"
+                key={idx}
+                className={styles.Alert}
+                dismissible
+              >
+                {message}
+              </Alert>
+            ))}
             <Form.Group controlId="password1">
               <Form.Label className="d-none">Create password</Form.Label>
               <Form.Control
                 type="password"
                 placeholder="Create password"
                 name="password1"
+                value={password1}
+                onChange={handleChange}
                 className={`mt-2 ${styles.FormInput}`}
               />
             </Form.Group>
+            {errors.password1?.map((message, idx) => (
+              <Alert
+                variant="info"
+                key={idx}
+                className={styles.Alert}
+                dismissible
+              >
+                {message}
+              </Alert>
+            ))}
             <Form.Group controlId="password2">
               <Form.Label className="d-none">Confirm password</Form.Label>
               <Form.Control
                 type="password"
                 placeholder="Confirm password"
                 name="password2"
+                value={password2}
+                onChange={handleChange}
                 className={`mt-2 ${styles.FormInput}`}
               />
             </Form.Group>
+            {errors.password2?.map((message, idx) => (
+              <Alert
+                variant="info"
+                key={idx}
+                className={styles.Alert}
+                dismissible
+              >
+                {message}
+              </Alert>
+            ))}
             <Button
               type="submit"
               className={`${btnStyles.Button} ${btnStyles.Large}`}
             >
               Sign up!
             </Button>
+            {/* {errors.non_field_errors?.map((message, idx) => (
+              <Alert
+                variant="info"
+                key={idx}
+                className={`mt-3 ${styles.Alert}`}
+                dismissible
+              >
+                {message}
+              </Alert>
+            ))} */}
           </Form>
         </Container>
         <Container className={`text-center mt-3 ${appStyles.Content}`}>
