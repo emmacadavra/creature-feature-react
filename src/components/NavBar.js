14316a830b1f7f16999d0c181fbb631ab82ea773
Emma Cadavra <emmacadavra@gmail.com>
2024-02-17 14:26:44 +0000

    Add styling to navbar links and change sign out icon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,28 +6,43 @@ import styles from "../styles/NavBar.module.css";
 import cf_logo_small from "../assets/cf_logo_small.png";
 import home from "../assets/home.png";
 import login from "../assets/login.png";
-import register from "../assets/register.png";
+import signup from "../assets/signup.png";
 
 const NavBar = () => {
   return (
     <Navbar expand="md" fixed="top" className={styles.NavBar}>
       <Container>
-        <Navbar.Brand>
+        <Navbar.Brand className={styles.NavBrand}>
           <img src={cf_logo_small} alt="Logo" height="60" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto text-end">
-            <Nav.Link>
-              <img src={home} alt="Home" height="32" />
+            <Nav.Link className={styles.NavLink}>
+              <img
+                src={home}
+                alt="Home"
+                height="32"
+                className={styles.NavIcon}
+              />
               Home
             </Nav.Link>
-            <Nav.Link>
-              <img src={login} alt="Sign In" height="32" />
+            <Nav.Link className={styles.NavLink}>
+              <img
+                src={login}
+                alt="Sign In"
+                height="32"
+                className={styles.NavIcon}
+              />
               Sign In
             </Nav.Link>
-            <Nav.Link>
-              <img src={register} alt="Sign Up" height="32" />
+            <Nav.Link className={styles.NavLink}>
+              <img
+                src={signup}
+                alt="Sign Up"
+                height="32"
+                className={styles.NavIcon}
+              />
               Sign Up
             </Nav.Link>
           </Nav>
