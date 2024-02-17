5772fa1325ee411393bf9bd3a03f98bea302f212
Emma Cadavra <emmacadavra@gmail.com>
2024-02-17 15:26:37 +0000

    Install react-router-dom and add Routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
+import Container from "react-bootstrap/Container";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container className={styles.Main}>
+        <Routes>
+          <Route path="/" element={<h1>Homepage</h1>} />
+          <Route path="/signin" element={<h1>Sign In</h1>} />
+          <Route path="/signup" element={<h1>Sign Up</h1>} />
+        </Routes>
+      </Container>
     </div>
   );
 }
