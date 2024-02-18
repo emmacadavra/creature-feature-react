9a0096d0c7c7aeacd2a50df990e7314797160eda
Emma Cadavra <emmacadavra@gmail.com>
2024-02-18 20:37:17 +0000

    Add function to CreatePost.js, adding images and form functionality (currently lacking styling)

--- src/pages/posts/CreatePost.js
+++ src/pages/posts/CreatePost.js
@@ -1,14 +1,69 @@
 import React, { useState } from "react";
-import { Form, Button, Col, Row, Container } from "react-bootstrap";
-import upload from "../../assets/upload.png";
+import { Form, Button, Col, Row, Container, Image } from "react-bootstrap";
+import uploadimage from "../../assets/upload.png";
+import camera from "../../assets/camera.png";
+import Asset from "../../components/Asset.js";
 
 const CreatePost = () => {
-  const [errors, setErrors] = useState({});
+  //   const [errors, setErrors] = useState({});
+
+  const [postData, setPostData] = useState({
+    title: "",
+    content: "",
+    image: "",
+    category: "",
+  });
+
+  const { title, content, image, category } = postData;
+
+  const handleChange = (event) => {
+    setPostData({
+      ...postData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleChangeImage = (event) => {
+    if (event.target.files.length) {
+      URL.revokeObjectURL(image);
+      setPostData({
+        ...postData,
+        image: URL.createObjectURL(event.target.files[0]),
+      });
+    }
+  };
 
   const postFormFields = (
     <div>
-      <Button>BUTTON</Button>
-      <Button>ANOTHER BUTTON</Button>
+      <Form.Group>
+        <Form.Label>Title</Form.Label>
+        <Form.Control
+          type="text"
+          name="title"
+          value={title}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Content</Form.Label>
+        <Form.Control
+          as="textarea"
+          rows={7}
+          name="content"
+          value={content}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Creature Category</Form.Label>
+        <Form.Select name="category" value={category} onChange={handleChange}>
+          <option value="fluffy">Facinorous Fluffballs</option>
+          <option value="scaly">Reptillian Villains</option>
+          <option value="feathers">Feathered Fiends</option>
+        </Form.Select>
+      </Form.Group>
+      <Button>Save Draft</Button>
+      <Button>Post Me!</Button>
     </div>
   );
 
@@ -18,13 +73,38 @@ const CreatePost = () => {
         <Col>
           <Container>
             <Form.Group>
-              <Form.Label>LABEL</Form.Label>
+              {image ? (
+                <>
+                  <figure>
+                    <Image src={image} rounded />
+                  </figure>
+                  <div>
+                    <Form.Label htmlFor="upload-image">
+                      <img src={camera} alt="Choose a new image" height="32" />
+                      Choose a different picture
+                    </Form.Label>
+                  </div>
+                </>
+              ) : (
+                <Form.Label htmlFor="upload-image">
+                  <Asset
+                    src={uploadimage}
+                    message="Click or tap to upload your image!"
+                  />
+                </Form.Label>
+              )}
+
+              <Form.Control
+                controlId="formFile"
+                id="upload-image"
+                className="d-none"
+                type="file"
+                accept="image/*"
+                onChange={handleChangeImage}
+              />
             </Form.Group>
             <div>{postFormFields}</div>
           </Container>
-        </Col>
-        <Col>
-          <Container></Container>
         </Col>
       </Row>
     </Form>
