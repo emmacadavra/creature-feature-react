08470532fa314716ae63f82257f5d9f9c1a4783c
Emma Cadavra <emmacadavra@gmail.com>
2024-02-23 14:41:13 +0000

    Post page set up, all parts showing as needed - ternary expression will need to be revised due to more complex logic needed for reactions

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -1,8 +1,18 @@
 import React from "react";
 import styles from "../../styles/Post.module.css";
 import { useAuth } from "../../contexts/AuthContext";
-import { Card, Image, OverlayTrigger, Tooltip } from "react-bootstrap";
+import { Card, OverlayTrigger, Tooltip } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import crowndefault from "../../assets/crown.png";
+import crowngreyscale from "../../assets/crown-greyscale.png";
+import crownhighlight from "../../assets/crown-highlight.png";
+import gooddefault from "../../assets/good.png";
+import goodgreyscale from "../../assets/good-greyscale.png";
+import goodhighlight from "../../assets/good-highlight.png";
+import lovedefault from "../../assets/love.png";
+import lovegreyscale from "../../assets/love-greyscale.png";
+import lovehighlight from "../../assets/love-highlight.png";
+import commentsimg from "../../assets/comments.png";
 import Avatar from "../../components/Avatar";
 
 const Post = (props) => {
@@ -15,7 +25,7 @@ const Post = (props) => {
     content,
     image,
     category,
-    reation_id,
+    reaction_id,
     comment_count,
     crown_count,
     good_count,
@@ -44,12 +54,59 @@ const Post = (props) => {
       </Link>
       <div>
         {is_owner ? (
-            <OverlayTrigger placement="top" overlay={<Tooltip>You can't react to your own posts!</Tooltip>}>
-                
-            </OverlayTrigger>
+          <OverlayTrigger
+            placement="top"
+            overlay={<Tooltip>You can&apos;t react to your own posts!</Tooltip>}
+          >
+            <span>
+              <img src={crowndefault} className={styles.Reactions} />
+              {crown_count}
+              <img src={gooddefault} className={styles.Reactions} />
+              {good_count}
+              <img src={lovedefault} className={styles.Reactions} />
+              {love_count}
+            </span>
+          </OverlayTrigger>
+        ) : reaction_id ? (
+          <span onClick={() => {}}>
+            <img src={crownhighlight} className={styles.Reactions} />
+            {crown_count}
+            <img src={goodhighlight} className={styles.Reactions} />
+            {good_count}
+            <img src={lovehighlight} className={styles.Reactions} />
+            {love_count}
+          </span>
+        ) : currentUser ? (
+          <span onClick={() => {}}>
+            <img src={crowndefault} className={styles.Reactions} />
+            {crown_count}
+            <img src={gooddefault} className={styles.Reactions} />
+            {good_count}
+            <img src={lovedefault} className={styles.Reactions} />
+            {love_count}
+          </span>
+        ) : (
+          <OverlayTrigger
+            placement="top"
+            overlay={
+              <Tooltip>You must be logged in to react to posts!</Tooltip>
+            }
+          >
+            <img src={crowngreyscale} className={styles.Reactions} />
+            {crown_count}
+            <img src={goodgreyscale} className={styles.Reactions} />
+            {good_count}
+            <img src={lovegreyscale} className={styles.Reactions} />
+            {love_count}
+          </OverlayTrigger>
         )}
+        <Link to={`/posts/${id}`}>
+          <img src={commentsimg} className={styles.Reactions} />
+          {comment_count}
+        </Link>
       </div>
       <Card.Body>
+        {category && <Card.Text>{category}</Card.Text>}
         {title && <Card.Title className="text-center">{title}</Card.Title>}
         {content && <Card.Text>{content}</Card.Text>}
       </Card.Body>
