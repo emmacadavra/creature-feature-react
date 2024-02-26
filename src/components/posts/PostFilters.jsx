3aa0686c7559c26ecaedbe565aab1278fe37b435
Emma Cadavra <emmacadavra@gmail.com>
2024-02-26 10:47:07 +0000

    Add react-bootstrap buttons for My Feed and My Faves

--- src/components/posts/PostFilters.jsx
+++ src/components/posts/PostFilters.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Form, Image } from "react-bootstrap";
+import { Button, Col, Container, Form, Image, Row } from "react-bootstrap";
 import styles from "../../styles/Posts.module.css";
 import search from "../../assets/search.png";
 import { useAuth } from "../../contexts/AuthContext";
@@ -9,55 +9,46 @@ export const PostFilters = ({ query, onQueryChange, onFilterChange }) => {
   const profile_id = currentUser?.profile_id || "";
 
   return (
-    <div>
-      <Image src={search} className={styles.SearchIcon} />
-      <Form
-        onSubmit={(event) => event.preventDefault()}
-        className={styles.SearchBar}
-      >
-        <Form.Control
-          type="text"
-          value={query}
-          onChange={onQueryChange}
-          className="me-sm-2"
-          placeholder="Search posts"
-        />
-      </Form>
-      <button
-        onClick={() => {
-          onFilterChange(`owner__followed__owner__profile=${profile_id}`);
-        }}
-      >
-        My friends
-      </button>
-      <button
-        onClick={() => {
-          onFilterChange("B");
-        }}
-      >
-        My likes
-      </button>
-      <button
-        onClick={() => {
-          onFilterChange("C");
-        }}
-      >
-        Fluffy
-      </button>
-      <button
-        onClick={() => {
-          onFilterChange("D");
-        }}
-      >
-        Scaly
-      </button>
-      <button
-        onClick={() => {
-          onFilterChange("E");
-        }}
-      >
-        Feathery
-      </button>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <Button
+            onClick={() => {
+              onFilterChange(`owner__followed__owner__profile=${profile_id}`);
+            }}
+          >
+            My Feed
+          </Button>
+        </Col>
+        <Col>
+          <Button
+            onClick={() => {
+              onFilterChange(
+                `reactions__owner__profile=${profile_id}&ordering=-reactions__created_on&`,
+              );
+            }}
+          >
+            My Faves
+          </Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Image src={search} className={styles.SearchIcon} />
+          <Form
+            onSubmit={(event) => event.preventDefault()}
+            className={styles.SearchBar}
+          >
+            <Form.Control
+              type="text"
+              value={query}
+              onChange={onQueryChange}
+              className="me-sm-2"
+              placeholder="Search posts"
+            />
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 };
